bdc47f907e1301de27089546d924868362f9accf
Sarah DeBey <sarah.debey@gmail.com>
2016-04-12 14:01:57 -0700

    adds console.log messages to track user answers

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ console.log('User name is ' + userName);
 alert('Okay ' + userName + ' , let\'s begin!');
 
 var answer1 = prompt('Do I like to code?').toUpperCase();
+console.log('Does ' + userName + ' think I like to code? ' + answer1);
 if(answer1 === 'Y' || answer1 === 'YES') {
   alert('Correct!');
   numCorrect += 1;
@@ -17,6 +18,7 @@ if(answer1 === 'Y' || answer1 === 'YES') {
 }
 
 var answer2 = prompt('Is my favorite number 47?').toUpperCase();
+console.log('Does ' + userName + ' think 47 is my fave number? ' + answer2);
 if(answer2 === 'Y' || answer2 === 'YES') {
   alert('Good Job ' + userName);
   numCorrect += 1;
@@ -26,6 +28,7 @@ if(answer2 === 'Y' || answer2 === 'YES') {
 }
 
 var answer3 = prompt('Is my favorite food salad?').toUpperCase();
+console.log('Does ' + userName + ' think salad is my favorite food? ' + answer3);
 if(answer3 === 'Y' || answer3 === 'YES') {
   alert('Of course not!');
   numWrong += 1;
@@ -35,6 +38,7 @@ if(answer3 === 'Y' || answer3 === 'YES') {
 }
 
 var answer4 = prompt('Do I have a dog named Piper?').toUpperCase();
+console.log('Does ' + userName + ' think I have a dog named Piper? ' + answer4);
 if(answer4 === 'Y' || answer4 === 'YES') {
   alert('Correct ' + userName);
   numCorrect += 1;
@@ -44,6 +48,7 @@ if(answer4 === 'Y' || answer4 === 'YES') {
 }
 
 var answer5 = prompt('Is this a good game?').toUpperCase();
+console.log('Does ' + userName + ' think this is a good game? ' + answer5);
 if(answer5 === 'Y' || answer5 === 'YES') {
   alert('Bingo!');
   numCorrect += 1;
